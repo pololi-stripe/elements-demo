6128604e00ec2cc00f3c61fdae55b74d79558119
Polo Li <pololi@stripe.com>
2023-01-24 17:50:14 -0800

    pl - add stripe multi-account + PE

---
+++ pages/api/create-confirm-intent.js
@@ -0,0 +1,28 @@
+const mxStripe = require("stripe")(process.env.MX_STRIPE_SK);
+const usStripe = require("stripe")(process.env.US_STRIPE_SK);
+
+export default async function handler(req, res) {
+  const { country, payment_method_id } = req.body;
+
+  const stripe = country === "MX" ? mxStripe : usStripe;
+  const currency = country === "MX" ? "mxn" : "usd";
+
+  try {
+    const intent = await stripe.paymentIntents.create({
+      confirm: true,
+      amount: 1099,
+      currency: currency,
+      automatic_payment_methods: { enabled: true },
+      payment_method: payment_method_id, // the PaymentMethod ID sent by your client
+      return_url: "https://example.com",
+    });
+    res.status(200).json({
+      client_secret: intent.client_secret,
+      status: intent.status,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: err,
+    });
+  }
+}
--- pages/demos/pe-multi-accounts.js
+++ pages/demos/pe-multi-accounts.js
@@ -2,11 +2,38 @@ import Head from "next/head";
 import Layout from "../../components/layout";
 import Select from "../../components/select";
 import { useState } from "react";
+import MxCheckout from "../../components/mx-checkout";
+import UsCheckout from "../../components/us-checkout";
 
 const countryOptions = ["USA", "Mexico"];
 
 export default function PaymentElementMultipleAccounts() {
-  const [country, setCountry] = useState("");
+  const [country, setCountry] = useState("USA");
+  const [paymentComplete, setPaymentComplete] = useState(false);
+  const markPaymentComplete = () => setPaymentComplete(true);
+
+  if (paymentComplete) {
+    return (
+      <Layout>
+        <Head>
+          <title>Demo</title>
+        </Head>
+        <h1 className="text-3xl mb-6">Payment Element + Multiple Accounts</h1>
+        <div
+          className="bg-orange-300 rounded-lg py-3 px-3 mb-4 text-base text-gray-800"
+          role="alert"
+        >
+          TEST MODE
+        </div>
+        <div
+          className="p-4 my-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
+          role="alert"
+        >
+          Payment Succeed!
+        </div>
+      </Layout>
+    );
+  }
 
   return (
     <Layout>
@@ -14,12 +41,23 @@ export default function PaymentElementMultipleAccounts() {
         <title>Demo</title>
       </Head>
       <h1 className="text-3xl mb-6">Payment Element + Multiple Accounts</h1>
+      <div
+        className="bg-orange-300 rounded-lg py-3 px-3 mb-4 text-base text-gray-800"
+        role="alert"
+      >
+        TEST MODE
+      </div>
       <h2 className="text-2xl mb-3">Country</h2>
       <Select
         selected={country}
         setSelect={setCountry}
         options={countryOptions}
       />
+      {country === "Mexico" ? (
+        <MxCheckout markPaymentComplete={markPaymentComplete} />
+      ) : (
+        <UsCheckout markPaymentComplete={markPaymentComplete} />
+      )}
     </Layout>
   );
 }
