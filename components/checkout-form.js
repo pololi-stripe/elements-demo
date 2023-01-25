e4bc4b07e4414c31be1f1d090989e35d85139907
Polo Li <pololi@stripe.com>
2023-01-24 18:01:19 -0800

    pl - fix padding

--- components/checkout-form.js
+++ components/checkout-form.js
@@ -19,7 +19,7 @@ const LoadingButton = () => {
     <button
       disabled
       type="button"
-      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
+      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 my-5 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
     >
       <svg
         aria-hidden="true"
