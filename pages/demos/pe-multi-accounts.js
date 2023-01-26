425cb0a6582399883b1209dade0b5cc750f5b447
Polo Li <pololi@stripe.com>
2023-01-26 14:39:18 -0800

    pl - multiple Elements in same component
    This does NOT work. When changing currency, it doesn't change Stripe
    instance

--- pages/demos/pe-multi-accounts.js
+++ pages/demos/pe-multi-accounts.js
@@ -1,62 +1,94 @@
 import Head from "next/head";
-import Layout from "../../components/layout";
-import Select from "../../components/select";
 import { useState } from "react";
-import MxCheckout from "../../components/mx-checkout";
-import UsCheckout from "../../components/us-checkout";
+import Layout from "../../components/layout";
+import TestModeBadge from "../../components/test-mode-badge";
+import CheckoutForm, { countryInEurope } from "../../components/checkout-form";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
-const countryOptions = ["USA", "Mexico"];
+const PaymentComplete = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>Demo</title>
+      </Head>
+      <h1 className="text-3xl mb-6">
+        <TestModeBadge />
+        Payment Element + Multiple Accounts
+      </h1>
+
+      <div
+        className="p-4 my-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
+        role="alert"
+      >
+        Payment Succeed!
+      </div>
+    </Layout>
+  );
+};
+
+const useEuAccount = (currency) => currency === "EUR";
+
+const usStripe = loadStripe(process.env.NEXT_PUBLIC_US_STRIPE_PK, {
+  betas: ["elements_enable_deferred_intent_beta_1"],
+});
+
+const euStripe = loadStripe(process.env.NEXT_PUBLIC_EU_STRIPE_PK, {
+  betas: ["elements_enable_deferred_intent_beta_1"],
+});
 
 export default function PaymentElementMultipleAccounts() {
-  const [country, setCountry] = useState("USA");
   const [paymentComplete, setPaymentComplete] = useState(false);
-  const markPaymentComplete = () => setPaymentComplete(true);
 
   if (paymentComplete) {
-    return (
-      <Layout>
-        <Head>
-          <title>Demo</title>
-        </Head>
-        <h1 className="text-3xl mb-6">Payment Element + Multiple Accounts</h1>
-        <div
-          className="bg-orange-300 rounded-lg py-3 px-3 mb-4 text-base text-gray-800"
-          role="alert"
-        >
-          TEST MODE
-        </div>
-        <div
-          className="p-4 my-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
-          role="alert"
-        >
-          Payment Succeed!
-        </div>
-      </Layout>
-    );
+    return <PaymentComplete />;
   }
+
+  const [currency, setCurrency] = useState("usd");
+  const [address, setAddress] = useState({});
+  const options = {
+    mode: "payment",
+    amount: 1099,
+    currency: currency,
+    appearance: { theme: "stripe" },
+  };
+
+  const handleAddressUpdate = (newAddress) => {
+    setAddress(newAddress);
+    if (!countryInEurope(newAddress.country)) {
+      setCurrency("usd");
+    }
+  };
 
   return (
     <Layout>
       <Head>
         <title>Demo</title>
       </Head>
-      <h1 className="text-3xl mb-6">Payment Element + Multiple Accounts</h1>
-      <div
-        className="bg-orange-300 rounded-lg py-3 px-3 mb-4 text-base text-gray-800"
-        role="alert"
-      >
-        TEST MODE
-      </div>
-      <h2 className="text-2xl mb-3">Country</h2>
-      <Select
-        selected={country}
-        setSelect={setCountry}
-        options={countryOptions}
-      />
-      {country === "Mexico" ? (
-        <MxCheckout markPaymentComplete={markPaymentComplete} />
+      <h1 className="text-3xl mb-6">
+        <TestModeBadge />
+        PE + Multiple Accounts
+      </h1>
+      {useEuAccount(currency) ? (
+        <Elements options={options} stripe={euStripe}>
+          <CheckoutForm
+            address={address}
+            handleAddressUpdate={handleAddressUpdate}
+            currency={currency}
+            setCurrency={setCurrency}
+            setPaymentComplete={setPaymentComplete}
+          />
+        </Elements>
       ) : (
-        <UsCheckout markPaymentComplete={markPaymentComplete} />
+        <Elements options={options} stripe={usStripe}>
+          <CheckoutForm
+            address={address}
+            handleAddressUpdate={handleAddressUpdate}
+            currency={currency}
+            setCurrency={setCurrency}
+            setPaymentComplete={setPaymentComplete}
+          />
+        </Elements>
       )}
     </Layout>
   );
