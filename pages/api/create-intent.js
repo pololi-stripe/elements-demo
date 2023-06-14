b790178c368be2b3624e81c64184d4dd0fa7f2cc
Polo Li <pololi@stripe.com>
2023-06-14 10:33:41 -0700

    pl - add optional amt demo

--- pages/api/create-intent.js
+++ pages/api/create-intent.js
@@ -2,16 +2,23 @@ const usStripe = require("stripe")(process.env.US_STRIPE_SK);
 const euStripe = require("stripe")(process.env.EU_STRIPE_SK);
 
 export default async function handler(req, res) {
-  const { currency } = req.body;
+  const { currency, mode } = req.body;
 
   const stripe = currency === "usd" ? usStripe : euStripe;
 
   try {
-    const intent = await stripe.paymentIntents.create({
-      amount: 1099,
-      currency: currency,
-      automatic_payment_methods: { enabled: true },
-    });
+    let intent;
+    if (mode === "setup") {
+      intent = await stripe.setupIntents.create({
+        automatic_payment_methods: { enabled: true },
+      });
+    } else {
+      intent = await stripe.paymentIntents.create({
+        amount: 1099,
+        currency: currency,
+        automatic_payment_methods: { enabled: true },
+      });
+    }
     res.status(200).json({
       client_secret: intent.client_secret,
       status: intent.status,
